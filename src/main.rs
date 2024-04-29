3bed71917dd3273efe166cb2bc7a67ae0869d094
LeviathanST <phamquochung170505@gmail.com>
2024-04-29 12:04:22 +0700

    ADD: struct UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod ui;
+
 use std::cmp::min;
 
 use pancurses::*;
+
+use ui::UI;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
@@ -18,33 +22,47 @@ fn main() {
     window.refresh();
     window.keypad(true);
 
-    let todo_list: Vec<String> = vec![
+    let mut todo_list: Vec<String> = vec![
         "Hello world with Rust".to_string(),
         "Love KH :3".to_string(),
         "Love KH :3".to_string(),
         "Love KH :3".to_string(),
     ];
 
-    let mut done = Vec::<String>::new();
+    let mut done_list: Vec<String> =
+        vec!["Hello world with Rust".to_string(), "Make todo".to_string()];
 
+    let mut done_cur: usize = 0;
     let mut todo_cur: usize = 0;
 
+    let mut ui = UI::default();
     let mut quit: bool = false;
     while !quit {
-        for (i, todo) in todo_list.iter().enumerate() {
-            let pair: i16 = {
-                if todo_cur == i {
-                    HIGHLIGHT_PAIR.into()
-                } else {
-                    REGULAR_PAIR.into()
-                }
-            };
+        window.erase();
+        ui.begin(0, 0);
+        {
+            ui.label("TODO: ", REGULAR_PAIR, &window);
+            ui.begin_list(todo_cur);
+            for (i, todo) in todo_list.iter().enumerate() {
+                ui.list_element(&window, &format!("- [ ] {}", todo), i);
+            }
+            ui.end_list();
 
-            window.attron(COLOR_PAIR(pair.try_into().unwrap()));
-            window.mv(i as i32, 1);
-            window.addstr(&todo);
-            window.attroff(COLOR_PAIR(pair.try_into().unwrap()));
+            ui.label(
+                "--------------------------------------------------",
+                REGULAR_PAIR,
+                &window,
+            );
+
+            ui.label("DONE: ", REGULAR_PAIR, &window);
+            ui.begin_list(done_cur);
+            for (i, done) in done_list.iter().enumerate() {
+                ui.list_element(&window, &format!("- [x] {}", done), i);
+            }
+
+            ui.end_list();
         }
+        ui.end();
 
         match window.getch() {
             Some(Input::Character('q')) => quit = true,
@@ -57,6 +75,12 @@ fn main() {
                 if (todo_cur < todo_list.len() - 1) {
                     todo_cur = min(todo_cur + 1, todo_list.len());
                 }
+            }
+            Some(Input::Character(' ')) => {
+                if (todo_cur >= 0) {
+                    todo_list.remove(todo_cur);
+                }
+                done_list.push(todo_list[todo_cur].clone());
             }
             _ => {}
         }
---
+++ src/ui.rs
@@ -0,0 +1,58 @@
+use pancurses::*;
+
+type Id = usize;
+
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
+#[derive(Default)]
+pub struct UI {
+    list_cur: Option<Id>,
+    row: usize,
+    col: usize,
+}
+
+impl UI {
+    pub fn begin(&mut self, row: usize, col: usize) {
+        self.row = row;
+        self.col = col;
+    }
+
+    pub fn begin_list(&mut self, id: Id) {
+        self.list_cur = Some(id);
+    }
+
+    pub fn list_element(&mut self, window: &Window, label: &str, id: Id) {
+        let id_cur = self
+            .list_cur
+            .expect("Not allowed to create list elements out side of lists");
+
+        self.label(
+            label,
+            {
+                if id_cur == id {
+                    HIGHLIGHT_PAIR.into()
+                } else {
+                    REGULAR_PAIR.into()
+                }
+            },
+            window,
+        );
+    }
+
+    pub fn label(&mut self, text: &str, pair: i16, window: &Window) {
+        window.mv(self.row.try_into().unwrap(), self.col.try_into().unwrap());
+        window.attron(COLOR_PAIR(pair.try_into().unwrap()));
+        window.addstr(text);
+        window.attroff(COLOR_PAIR(pair.try_into().unwrap()));
+        self.row += 1;
+    }
+
+    pub fn end_list(&mut self) {
+        self.list_cur = None;
+    }
+
+    pub fn end(&mut self) {
+        self.list_cur = None;
+    }
+}
